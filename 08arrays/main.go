f44ac5ff5218f19e3c85b14328c7176e287ba04c
Uday Sai Tyada <udaysai2551998@gmail.com>
2024-12-03 00:48:56 +0530

    arrays

---
+++ 08arrays/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to the lesson on arrays")
+
+	var fruitList [5]string
+
+	fruitList[0] = "Apple"
+	fruitList[1] = "Orange"
+	fruitList[2] = "Banana"
+	fruitList[3] = "Papaya"
+	fruitList[4] = "Tomato"
+
+	fmt.Println("Fruit list is : ", fruitList)
+	fmt.Println("Fruit list length is : ", len(fruitList))
+
+	var vegetableList = [3]string{"carrot", "beetroot", "cabbage"}
+
+	fmt.Println("Vegetable list is : ", vegetableList)
+	fmt.Println("Vegetable list length is : ", len(vegetableList))
+
+	var array = [5]int{1, 2, 3, 4, 5}
+	fmt.Println(array)
+}
